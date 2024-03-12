2230820374c663816ddbcd7e42dee6b5185e6e7a
Xuhanzhuo <144585884+Xuhanzhuo@users.noreply.github.com>
2024-03-12 22:46:34 +0800

    new

---
+++ Assets/Scripts/Enemy/EnemyMaleZombie.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMaleZombie : MonoBehaviour
+{
+
+    public Vector3 targetPosition;
+    public float mySpeed;
+    Animator myAnim;
+    Vector3 originPosition, turnPoint;
+    GameObject myPlayer;
+
+
+
+
+
+    // Start is called before the first frame update
+
+
+    private void Awake()
+    {
+        myAnim = GetComponent<Animator>();
+        originPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        myPlayer = GameObject.Find("Player");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(myPlayer.transform.position, transform.position) < 1.5f)
+        {
+            if(myPlayer.transform.position.x <= transform.position.x)
+            {
+                transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+            }
+
+            if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attact"))
+            {
+                return;
+            }
+            myAnim.SetTrigger("Attact");
+            return;
+        }
+        else
+        {
+            if (turnPoint == targetPosition)
+            {
+                transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            }
+            else if (turnPoint == originPosition)
+            {
+                transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            }
+        }
+        if(transform.position.x == targetPosition.x)
+        {
+            myAnim.SetTrigger("Idle");
+            turnPoint = originPosition;
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+        else if (transform.position.x == originPosition.x)
+        {
+            myAnim.SetTrigger("Idle");
+            turnPoint = targetPosition;
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        
+        if(myAnim.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, turnPoint, mySpeed * Time.deltaTime);
+        }
+        
+    }
+}
