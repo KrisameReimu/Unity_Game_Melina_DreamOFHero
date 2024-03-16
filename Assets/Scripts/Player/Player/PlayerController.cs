61adab48df7c9f21be77aa9d72dbb60438a4ef8a
beeeeeeen9677 <107660645+beeeeeeen9677@users.noreply.github.com>
2024-03-16 14:36:32 +0800

    .

--- Assets/Scripts/Player/Player/PlayerController.cs
+++ Assets/Scripts/Player/Player/PlayerController.cs
@@ -156,7 +156,7 @@ public class PlayerController : MonoBehaviour
         float upperAngle = anim.GetBool("isLookUp") ? 1f : 0f;
         GameObject boltObject = Instantiate(boltPrefab, rb.position + new Vector2(direction*(1+0.4f*hight), 1-0.6f*hight+0.4f * upperAngle), Quaternion.Euler(new Vector3(0, 0, 90 + (90+upperAngle*30) * direction)));
         Bolt bolt = boltObject.GetComponent<Bolt>();
-        bolt.Shoot(new Vector2(direction, upperAngle*0.3f), 300);
+        bolt.Shoot(new Vector2(direction, upperAngle*0.4f), 300);
     }
 
     private void Cooldown()
